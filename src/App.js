31dada646df1d645d71babb560eb88493094dca6
andrii-pozniak <andriipozniak@gmail.com>
2023-07-31 23:07:23 +0300

    add header table

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
+import { Table } from './components/Table/Table';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <h2>
-        TASK1
-      </h2>
-
+    <div>
+      <Table/>
+      
     </div>
   );
 }
---
+++ src/components/Table/Table.style.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+max-width: 1170px;
+margin-right: auto;
+margin-left: auto;
+padding: 20px;
+`;
+export const TabStyle = styled.table`
+width: 100%;
+`;
+export const HeaderTab = styled.thead`
+background: grey;
+color: white;
+`;
+export const CellTab = styled.th`
+padding: 10px;
+`;
