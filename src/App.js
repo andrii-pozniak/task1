c3c7782ac013faa62710f8f627ec9dc3024f3eef
andrii-pozniak <andriipozniak@gmail.com>
2023-07-31 20:13:26 +0300

    create task1

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+        <p>
+          TASK1
+        </p>
+        
+      
+    </div>
+  );
+}
+
+export default App;
