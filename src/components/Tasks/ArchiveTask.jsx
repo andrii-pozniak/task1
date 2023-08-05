3c17a6aa3e50a1be6f19834c2706c34300362eb0
andrii-pozniak <andriipozniak@gmail.com>
2023-08-05 17:53:21 +0300

    change archived

--- src/components/Tasks/ArchiveTask.jsx
+++ src/components/Tasks/ArchiveTask.jsx
@@ -18,28 +18,38 @@ export const ArchivedTasks = ({ tasks, archivedTasks }) => {
     ).length;
     return { active: activeCount, archived: archivedCount };
   };
+  const categories = ["Task", "Idea", "Quot", "Random Thought"];
   return (
     <>
       <ContainerTasks
-        style={{ background: " grey", borderRadius: "5px", marginTop: "50px" }}
+        style={{ background: "grey", borderRadius: "5px", marginTop: "50px" }}
       >
         <Item>Name</Item>
         <Item>Active</Item>
         <Item>Archived</Item>
       </ContainerTasks>
-      {["Task", "Idea", "Quot", "Random Thought"].map((category) => (
-        <ContainerTasks key={category} style={{ background: "#e6e6ff" }}>
-          <Item>
-            {category === "Task" && <ShoppingCartIconStyle />}
-            {category === "Idea" && <EmojiObjectsIconStyle />}
-            {category === "Quot" && <FormatQuoteIconStyle />}
-            {category === "Random Thought" && <PsychologyIconStyle />}
-            {category}
-          </Item>
-          <Item>{countTasksByCategory(category).active}</Item>
-          <Item>{countTasksByCategory(category).archived}</Item>
-        </ContainerTasks>
-      ))}
+      {categories.map((category) => {
+        const activeCount = countTasksByCategory(category).active;
+        const archivedCount = countTasksByCategory(category).archived;
+
+        if (activeCount === 0 && archivedCount === 0) {
+          return null;
+        }
+
+        return (
+          <ContainerTasks key={category} style={{ background: "#e6e6ff" }}>
+            <Item>
+              {category === "Task" && <ShoppingCartIconStyle />}
+              {category === "Idea" && <EmojiObjectsIconStyle />}
+              {category === "Quot" && <FormatQuoteIconStyle />}
+              {category === "Random Thought" && <PsychologyIconStyle />}
+              {category}
+            </Item>
+            <Item>{activeCount}</Item>
+            <Item>{archivedCount}</Item>
+          </ContainerTasks>
+        );
+      })}
     </>
   );
 };
